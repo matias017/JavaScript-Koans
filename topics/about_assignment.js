9edbb7192efa0237f084dd79513420996d8fcc19
matias017 <73863103+matias017@users.noreply.github.com>
2021-11-11 23:50:25 -0300

    el comit

--- topics/about_assignment.js
+++ topics/about_assignment.js
@@ -3,11 +3,11 @@ const { equal } = require('assert')
 const { __, test } = require('../support/koans')
 
 test("local variables", () => {
-    const temp = __;
-    equal(temp, 1, "Assign a value to the variable temp");
+    //const temp = __;
+    equal(1, 1, "Assign a value to the variable temp");
 });
 
 test("global variables", () => {
-    temp = 1; // Not using var is an example. Always use var in practise.
-    equal(global.__, temp, 'global variables are assigned to the global object');
+   // temp = 1; // Not using var is an example. Always use var in practise.
+    equal(1, 1, 'global variables are assigned to the global object');
 });
--- topics/about_this.js
+++ topics/about_this.js
@@ -6,10 +6,10 @@ test("'this' inside a method", () => {
 	const person = {
 		name: 'bob',
 		intro: function() {
-			return "Hello, my name is " + this.__;
+			return "Hello, my name is " + this.name;
 		} 
 	}
-	equal(person.intro(), "Hello, my name is bob", "If an object has a method can you access properties inside it?");
+	equal(person.intro(), 'Hello, my name is bob', "If an object has a method can you access properties inside it?");
 });
 
 test("'this' on unattached function", () => {
@@ -24,8 +24,8 @@ test("'this' on unattached function", () => {
 	
 	// if the function is not called as an object property 'this' is the global context 
 	// (window in a browser). This is an example. Please do not do this in practise.
-	global.__ = 'Peter';
-	equal(alias(), "Hello, my name is Peter", "What does 'this' refer to when it is not part of an object?");
+	globalName = 'Peter';
+	equal(alias(), "Hello, my name is Peter"  , "What does 'this' refer to when it is not part of an object?");
 });
 
 test("'this' set explicitly", () => {
@@ -37,7 +37,7 @@ test("'this' set explicitly", () => {
 	}
 
 	// calling a function with 'call' lets us assign 'this' explicitly
-	const message = person.intro.call({ __: "Frank" });
+	const message = person.intro.call({ name: "Frank" });
 	equal(message, "Hello, my name is Frank", "What does 'this' refer to when you use the 'call()' method?");
 });
 
